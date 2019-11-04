c882a010b6dbe180174d7a7894e04a6c7e645116
Alexander Erni <alexander.c.erni@gmail.com>
2019-11-04 15:33:52 +0100

    quickfix

--- src/screens/CharacterInfoScreen/CharacterInfoScreen.tsx
+++ src/screens/CharacterInfoScreen/CharacterInfoScreen.tsx
@@ -1,38 +1,39 @@
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
 
-import React from 'react';
-import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
-
-import CharacterImage from '../../components/CharacterImage'; 
+import CharacterImage from '../../components/CharacterImage';
 import NameField from '../../components/NameField';
 import HeritageField from '../../components/HeritageField';
 import ParagonPrimalField from '../../components/ParagonPrimalField';
 import BackgroundField from '../../components/SceneField';
 import ArchetypeField from '../../components/ArchetypeField';
 
-
 interface Props {}
 
 function CharacterInfoScreen(props: Props) {
   const {} = props;
-  const [name, setName] = useState('')
-  const [heritage, setHeritage] = useState('')
-  const [paragonPrimal, setParagonPrimal] = useState('')
-  const [background, setBackground] = useState('')
-  const [archetype, setArchetype] = useState('')
+  const [name, setName] = useState('');
+  const [heritage, setHeritage] = useState('');
+  const [paragonPrimal, setParagonPrimal] = useState('');
+  const [background, setBackground] = useState('');
+  const [archetype, setArchetype] = useState('');
 
   return (
-    <View>
-    {console.log(`Name: ${name}, heritage: ${heritage}, paragon/primal: ${paragonPrimal}, background: ${background}, archetype: ${archetype}`)}
+    <View style={{flex: 1, alignSelf: 'center'}}>
+      {console.log(
+        `Name: ${name}, heritage: ${heritage}, paragon/primal: ${paragonPrimal}, background: ${background}, archetype: ${archetype}`,
+      )}
       <CharacterImage />
-      <Text>WHO R U</Text>
-      <NameField setName={setName}/>
-      <HeritageField setHeritage={setHeritage}/>
-      <ParagonPrimalField heritage={heritage} setParagonPrimal={setParagonPrimal}/>
-      <BackgroundField setBackground={setBackground}/>
-      <ArchetypeField setArchetype={setArchetype}/>
+      <NameField setName={setName} />
+      <HeritageField setHeritage={setHeritage} />
+      <ParagonPrimalField
+        heritage={heritage}
+        setParagonPrimal={setParagonPrimal}
+      />
+      <BackgroundField setBackground={setBackground} />
+      <ArchetypeField setArchetype={setArchetype} />
     </View>
-  )
+  );
 }
 
 export default CharacterInfoScreen;
