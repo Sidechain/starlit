941f799b3d4d24203f67b52630f2df63e280325a
MrEmperawr <marcus.kalmar@gmail.com>
2019-11-04 12:12:03 +0100

    Put fields in place

--- src/screens/CharacterInfoScreen/CharacterInfoScreen.tsx
+++ src/screens/CharacterInfoScreen/CharacterInfoScreen.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+
+import NameField from '../../components/NameField';
+import HeritageField from '../../components/HeritageField';
+import ParagonPrimalField from '../../components/ParagonPrimalField';
+import BackgroundField from '../../components/SceneField';
+import ArchetypeField from '../../components/ArchetypeField';
 
 interface Props {}
 
 function CharacterInfoScreen(props: Props) {
   const {} = props;
+  const [name, setName] = useState('')
+  const [heritage, setHeritage] = useState('')
+  const [paragonPrimal, setParagonPrimal] = useState('')
+  const [background, setBackground] = useState('')
+  const [archetype, setArchetype] = useState('')
 
-  return <Text>WHO R U</Text>;
+  return (
+    <View>
+      <Text>WHO R U</Text>
+      <NameField/>
+      <HeritageField/>
+      <ParagonPrimalField/>
+      <BackgroundField/>
+      <ArchetypeField/>
+    </View>
+  )
 }
 
 export default CharacterInfoScreen;
