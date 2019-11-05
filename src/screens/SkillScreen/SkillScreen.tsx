4bc2c1ced9a8c25e2db6df8e8645d5c751df0ab9
MrEmperawr <marcus.kalmar@gmail.com>
2019-11-05 16:34:58 +0100

    More styling

--- src/screens/SkillScreen/SkillScreen.tsx
+++ src/screens/SkillScreen/SkillScreen.tsx
@@ -11,114 +11,118 @@ function SkillScreen(props: Props) {
   return (
     <View style={{flex: 1, flexWrap: 'wrap'}}>
       <View>
-        <Text style={{fontSize: 16}}>Athletics Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'athletic')
-          .map(skillGroup => {
-            return (
+        <View>
+          <Text style={{fontSize: 16}}>Athletics Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'athletic')
+            .map(skillGroup => {
+              return (
+                <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Combat Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'combat')
+            .map(skillGroup => {
+              return (
               <Text>{skillGroup.name}</Text>
-            )
-          })}
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Creative Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'creative')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Knowledge Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'knowledge')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Language Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'language')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
       </View>
-      <View>
-        <Text style={{fontSize: 16}}>Combat Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'combat')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Creative Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'creative')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Knowledge Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'knowledge')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Language Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'language')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Magic Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'magic')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Magic Traditions</Text>
-        {skills
-          .filter(skill => skill.group === 'traditions')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Perceptive Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'perceptive')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Social Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'social')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Subterfuge Skills</Text>
-        {skills
-          .filter(skill => skill.group === 'subterfuge')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
-      </View>
-      <View>
-        <Text style={{fontSize: 16}}>Wilderness skills</Text>
-        {skills
-          .filter(skill => skill.group === 'wilderness')
-          .map(skillGroup => {
-            return (
-            <Text>{skillGroup.name}</Text>
-            )
-          })}
+      <View style={{marginLeft: 50}}>
+        <View>
+          <Text style={{fontSize: 16}}>Magic Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'magic')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Magic Traditions</Text>
+          {skills
+            .filter(skill => skill.group === 'tradition')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Perceptive Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'perceptive')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Social Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'social')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Subterfuge Skills</Text>
+          {skills
+            .filter(skill => skill.group === 'subterfuge')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
+        <View>
+          <Text style={{fontSize: 16}}>Wilderness skills</Text>
+          {skills
+            .filter(skill => skill.group === 'wilderness')
+            .map(skillGroup => {
+              return (
+              <Text>{skillGroup.name}</Text>
+              )
+            })}
+        </View>
       </View>
     </View>
     
