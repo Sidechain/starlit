9425102a34c9e6957ac880578298a909fa8c9b09
MrEmperawr <marcus.kalmar@gmail.com>
2019-11-05 11:18:45 +0100

    Refactor AttributeField

--- src/components/AttributeField/AttributeField.tsx
+++ src/components/AttributeField/AttributeField.tsx
@@ -4,15 +4,19 @@ import {Text, Button, View} from 'react-native';
 const max = 18;
 const min = 3;
 
-const AttributeField = (props: props) => {
-  let [agility, setAgility] = useState(0);
-  let [charisma, setCharisma] = useState(0);
-  let [constitution, setConstitution] = useState(0);
-  let [dexterity, setDexterity] = useState(0);
-  let [intelligence, setIntelligence] = useState(0);
-  let [perception, setPerception] = useState(0);
-  let [strength, setStrength] = useState(0);
-  let [will, setWill] = useState(0);
+interface Props {
+  
+}
+
+const AttributeField = (props: Props) => {
+  const [agility, setAgility] = useState(0);
+  const [charisma, setCharisma] = useState(0);
+  const [constitution, setConstitution] = useState(0);
+  const [dexterity, setDexterity] = useState(0);
+  const [intelligence, setIntelligence] = useState(0);
+  const [perception, setPerception] = useState(0);
+  const [strength, setStrength] = useState(0);
+  const [will, setWill] = useState(0);
 
   return (
     <View>
@@ -29,33 +33,33 @@ const AttributeField = (props: props) => {
         title="roll All"
         onPress={() => {
           setAgility(
-            (agility = Math.floor(Math.random() * (max - min + 1) + min)),
+            (Math.floor(Math.random() * (max - min + 1) + min)),
           );
           setCharisma(
-            (charisma = Math.floor(Math.random() * (max - min + 1) + min)),
+            (Math.floor(Math.random() * (max - min + 1) + min)),
           );
 
           setConstitution(
-            (constitution = Math.floor(Math.random() * (max - min + 1) + min)),
+            (Math.floor(Math.random() * (max - min + 1) + min)),
           );
 
           setDexterity(
-            (dexterity = Math.floor(Math.random() * (max - min + 1) + min)),
+            (Math.floor(Math.random() * (max - min + 1) + min)),
           );
 
           setIntelligence(
-            (intelligence = Math.floor(Math.random() * (max - min + 1) + min)),
+            (Math.floor(Math.random() * (max - min + 1) + min)),
           );
 
           setPerception(
-            (perception = Math.floor(Math.random() * (max - min + 1) + min)),
+            (Math.floor(Math.random() * (max - min + 1) + min)),
           );
 
           setStrength(
-            (strength = Math.floor(Math.random() * (max - min + 1) + min)),
+            (Math.floor(Math.random() * (max - min + 1) + min)),
           );
 
-          setWill((will = Math.floor(Math.random() * (max - min + 1) + min)));
+          setWill((Math.floor(Math.random() * (max - min + 1) + min)));
         }}
       />
     </View>
