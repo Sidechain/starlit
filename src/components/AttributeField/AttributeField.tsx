4086d7abece1a81522b8767dee84e87b2136e800
MrEmperawr <marcus.kalmar@gmail.com>
2019-11-05 11:09:56 +0100

    Remove outcommented code and unnecessary roll buttons

--- src/components/AttributeField/AttributeField.tsx
+++ src/components/AttributeField/AttributeField.tsx
@@ -26,76 +26,6 @@ const AttributeField = (props: props) => {
       <Text>Will: {will}</Text>
 
       <Button
-        title="roll Agility"
-        onPress={() => {
-          setAgility(
-            (agility = Math.floor(Math.random() * (max - min + 1) + min)),
-          );
-        }}
-      />
-
-      <Button
-        title="roll Charisma"
-        onPress={() => {
-          setCharisma(
-            (charisma = Math.floor(Math.random() * (max - min + 1) + min)),
-          );
-        }}
-      />
-
-      <Button
-        title="roll Constitution"
-        onPress={() => {
-          setConstitution(
-            (constitution = Math.floor(Math.random() * (max - min + 1) + min)),
-          );
-        }}
-      />
-
-      <Button
-        title="roll Dexterity"
-        onPress={() => {
-          setDexterity(
-            (dexterity = Math.floor(Math.random() * (max - min + 1) + min)),
-          );
-        }}
-      />
-
-      <Button
-        title="roll Intelligence"
-        onPress={() => {
-          setIntelligence(
-            (intelligence = Math.floor(Math.random() * (max - min + 1) + min)),
-          );
-        }}
-      />
-
-      <Button
-        title="roll Perception"
-        onPress={() => {
-          setPerception(
-            (perception = Math.floor(Math.random() * (max - min + 1) + min)),
-          );
-        }}
-      />
-
-      <Button
-        title="roll Strength"
-        onPress={() => {
-          setStrength(
-            (strength = Math.floor(Math.random() * (max - min + 1) + min)),
-          );
-        }}
-      />
-
-      <Button
-        title="roll Will"
-        onPress={() => {
-          setWill((will = Math.floor(Math.random() * (max - min + 1) + min)));
-        }}
-      />
-
-      <Button
         title="roll All"
         onPress={() => {
           setAgility(
@@ -128,48 +58,6 @@ const AttributeField = (props: props) => {
           setWill((will = Math.floor(Math.random() * (max - min + 1) + min)));
         }}
       />
-
-      {/* {attributesObject.map(e => {
-        let name = Object.keys(e);
-        let value = Object.values(e);
-        return (
-          <>
-            <Text>
-              {Object.keys(e)} {Object.values(e)}
-            </Text>
-            <Button
-              title={`increase ${Object.keys(e)}`}
-              onPress={() => {
-                console.log('this is the name ', ...name);
-                console.log('this is the value ', ...value);
-              }}
-            />
-          </>
-        );
-      })} */}
-      {/* <Text>Agility: {attributes.Agility}</Text>
-      <Button title="roll Agility" onPress={() => null} />
-
-      <Text>Charisma: {attributes.Charisma}</Text>
-      <Button title="roll Charisma" onPress={() => null} />
-
-      <Text>Constitution: {attributes.Constitution}</Text>
-      <Button title="roll Constitution" onPress={() => null} />
-
-      <Text>Dexterity: {attributes.Dexterity}</Text>
-      <Button title="roll Dexterity" onPress={() => null} />
-
-      <Text>Intelligence: {attributes.Intelligence}</Text>
-      <Button title="roll Intelligence" onPress={() => null} />
-
-      <Text>Perception: {attributes.Perception}</Text>
-      <Button title="roll Perception" onPress={() => null} />
-
-      <Text>Strength: {attributes.Strength}</Text>
-      <Button title="roll Strength" onPress={() => null} />
-
-      <Text>Will: {attributes.Will}</Text>
-      <Button title="roll Will" onPress={() => null} /> */}
     </View>
   );
 };
