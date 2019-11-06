35c69a2d0c36c24c8e45545ff13bd2eb4bd9627c
Alexander Erni <alexander.c.erni@gmail.com>
2019-11-06 10:48:22 +0100

    cleaning up

--- App.js
+++ App.js
@@ -2,11 +2,6 @@ import React from 'react';
 import {AppContainer} from './src/utils/navigationRoutes';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {activeTab: 'start'};
-  }
-
   render() {
     return <AppContainer />;
   }
