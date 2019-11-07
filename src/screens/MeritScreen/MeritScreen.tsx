f9c3d05cc5a5c87d3fea7e77681d8c25238faf9d
Alexander Erni <alexander.c.erni@gmail.com>
2019-11-07 11:57:37 +0100

    updated meritcards

--- src/screens/MeritScreen/MeritScreen.tsx
+++ src/screens/MeritScreen/MeritScreen.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import MeritCard from '../../components/MeritCard';
-import {ScrollView, View} from 'react-native';
+import {View, ScrollView} from 'react-native';
+import {merits} from '../../utils/meritCardData';
 
 interface Props {}
 
 function MeritScreen(props: Props) {
   const {} = props;
 
-  return <MeritCard />;
+  return (
+    <ScrollView>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        {merits.map((item, index) => {
+          return <MeritCard title={item.title} content={item.content} />;
+        })}
+      </View>
+    </ScrollView>
+  );
 }
 
 export default MeritScreen;
