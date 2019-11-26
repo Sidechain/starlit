9d95ba29a43333630129bd44635c01dc2014fd75
MrEmperawr <marcus.kalmar@gmail.com>
2019-11-26 15:52:54 +0100

    Set up summary screen for backend

--- src/screens/SummaryScreen/SummaryScreen.tsx
+++ src/screens/SummaryScreen/SummaryScreen.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import {Text} from 'react-native'
+import axios from 'axios'
+import { View } from 'native-base';
+
 interface Props {}
 
 function SummaryScreen(props: Props) {
+  const characterInfo = props.navigation.getParam('characterInfo');
+  const attributeData = props.navigation.getParam('attributeData');
+  const skills = props.navigation.getParam('skills');
+  const merits = props.navigation.getParam('merits');
 
+    const characterSummary = {
+      characterInfo: characterInfo,
+      attributes: attributeData,
+      skills: skills,
+      merits: merits,
+    }
     return (
-      <Text>WOW, YOU KINDA MADE A CHARACTER! LOOK AT ALL THOSE STATS... Eeeerhm... Yeah...</Text>
+        <View>
+          <Text>WOW, YOU KINDA MADE A CHARACTER! LOOK AT ALL THOSE STATS... Eeeerhm... Yeah...</Text>
+        </View>
     )
 }
 
